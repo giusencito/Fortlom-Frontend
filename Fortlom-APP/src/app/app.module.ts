40c6f050cb72d161fff06e59f2ce9bdf0ddfedfb
Alejandro <huamanalejandro97@gmail.com>
2021-09-23 18:59:40 -0500

    Angular Material Installed

--- Fortlom-APP/src/app/app.module.ts
+++ Fortlom-APP/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DemoMaterialModule } from './material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { DemoMaterialModule } from './material';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DemoMaterialModule
+    DemoMaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
