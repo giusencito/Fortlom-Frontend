5e6888efb64ffe1fc205c552235ad3bd64152ee6
eduperaltas <61795886+eduperaltas@users.noreply.github.com>
2021-09-30 23:23:49 -0500

    feat: Final version of forum
    User storie 07A

--- Fortlom-APP/src/app/app-routing.module.ts
+++ Fortlom-APP/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { FanaticForumComponent } from './Fanatic-Forum/Fanatic-Forum.component';
 
-const routes: Routes = [];
+
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import { EventComponent } from './event/event.component';
+import {ArtistForumComponent} from "./artist-forum/artist-forum.component";
+
+const routes: Routes = [
+{path : 'Home', component: HomeComponent},
+{path:'fanaticforum',component:FanaticForumComponent},
+{path:'Event',component:EventComponent},
+{path:'artistforum',component:ArtistForumComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
